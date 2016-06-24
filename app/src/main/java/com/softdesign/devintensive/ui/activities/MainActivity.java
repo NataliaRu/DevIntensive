129eac64dd7b435c27a33444d70e59bab17ff719
NataliaRu <bogdanova_80@mail.ru>
2016-06-24 19:53:08 +0600

    //верстка экрана приложения

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
-                }
+    }
 
     @Override
     protected void onStart() {
